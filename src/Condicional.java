e148be641e879d142cd60be2985c4c45c57da7c8
Gianluca Bonatto <gianluca.bonatto1@gmail.com>
2024-04-09 16:31:50 -0300

    c4 - continuando a aula de java

---
+++ src/Condicional.java
@@ -0,0 +1,23 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        System.out.println("Data de lançamento do filme: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do filme: " + notaDoFilme);
+
+        String tipoPlano = "Plus";
+
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Lançamento que os clientes estão curtindo!");            
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("Plus")) {
+            System.out.println("Filme liberado!");
+        } else {
+            System.out.println("Faça a locação do filme.");
+        }
+    }
+}
