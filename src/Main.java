6d2f68910d1f3bc841031ddc96c4b93017ea47b0
Gianluca Bonatto <gianluca.bonatto1@gmail.com>
2024-04-09 14:23:03 -0300

    c2 - aula 1 java, parte 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        
+        // boolean incluidoNoPlano = true;
+        // System.out.println(incluidoNoPlano);
+
+        // NOTA DO FILME
+        // double notaDoFilme = 8.1;
+        // System.out.println(notaDoFilme); 
+        
+        String sinopse;
+        sinopse = """
+                Filme de ação com um doidão ai.
+                Falam que é bom, mas nunca vi e é isso msm.
+                Nota do filme: 8.1
+                """;
+        System.out.println(sinopse);                
+    }
+}
+
+
