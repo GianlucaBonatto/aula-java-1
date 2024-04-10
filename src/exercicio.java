e209887f1d0c2bf1b9dee7927d4a4aec3ac3e348
Gianluca Bonatto <gianluca.bonatto1@gmail.com>
2024-04-10 11:35:24 -0300

    c7 - continuando a aula...

---
+++ src/exercicio.java
@@ -0,0 +1,10 @@
+public class exercicio {
+  public static void main(String[] args) {
+    int contador = 1;
+    
+    while(contador <= 10) {
+    System.out.println(contador++);
+   }
+}    
+}
+    // CONTADOR DE 1 ATE 10
